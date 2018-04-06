d3e2fdc0b55826d1aa1d6790c858aa191a2daba2
= <jardriel.redes18@gmail.com>
2018-04-06 01:48:06 -0300

    Projeto Cadastro - Melhoramento no Front e Organização

--- cadastro.php
+++ cadastro.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Cadastro</title>
-	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -18,11 +18,11 @@
 	<div class="container-fluid">
 		
 		<div class="row">
-			<div class="col-md-offset- borda">
+			<div class=" banner" id="box">
 
-				<form action="inseri.php" method="POST" class="col-sm-offset-4 col-xs-offset-1 ">
+				<form action="inseri.php" method="POST" class="col-sm-offset- col-xs-offset- ">
 					<h1>Cadastro</h1>
-					<div class="form-group col-md-5 col-sm-6 col-xs-10">
+					<div class="form-group col-md-8">
 						<input type="text" placeholder="nome" name="nome" id="nome"  class="form-control" required/><br>
 						<input type="email" placeholder="email" name="email" class="form-control"><br>
 						<input type="password" placeholder="senha" name="senha" class="form-control"><br>
--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Cadastro</title>
-	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/login.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -20,15 +20,15 @@
 	<div class="container-fluid">
 		
 		<div class="row">
-			<div class="col-md-offset-4">
+			<div class=" banner " id="box">
 
 				<form action="valida.php" method="POST" class="borda">
 					<h1>Login</h1>
-					<div class="form-group col-md-5">
+					<div class="form-group col-md-6">
 					
 						<input type="email" placeholder="email" name="email" class="form-control"><br>
 						<input type="password" placeholder="senha" name="senha" class="form-control"><br>
-						<input type="submit" value="enviar" class="btn btn-primary col-md-12"><br>
+						<input type="submit" value="enviar" class="btn btn-primary col-md-12 btn-lg"><br>
 						<p class="text-center text-danger">
 					<?php 
 						if(isset($_SESSION['LoginErro'])){
