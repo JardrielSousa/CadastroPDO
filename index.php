e4a1c2d211f838680403ce7dbbc115d2ea153217
jardriel Sousa <jardriel.redes18@gmail.com>
2018-04-05 23:47:32 -0300

    Projeto Cadastro - Front end Login

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/bootstrap-theme.css">
-  <link rel="stylesheet" href="css/cadastro.css">
+  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="js/jquery.js"></script>
   <script src="js/bootstrap.js"></script>
@@ -34,35 +34,21 @@
  </head>
  <body>
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Brand</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-	<div class="center">
-		<div class=" pull-right ">
-		<a href="login.php" class="btn-lg btn-primary">Login</a>
-		</div>	
-		<div class=" pull-right">
-		<a href="cadastro.php" class="btn-lg btn-default">Cadastro</a>
+	<div class="banner" id="box">
+		<div class="text ">
+			<h1 class="text">Sejam Bem Vindos</h1>
+			<p class="text">Selecionem uma Opção:</p>
 		</div>
+		<div class="text-center ">
+			<div class="row col-md-2">
+			<a href="login.php" class="btn-lg btn-primary">Login</a>
+			</div>	
+			<div class="row col-md-offset-4">
+			<a href="cadastro.php" class="btn-lg btn-default">Cadastro</a>
+			</div>
+		</div>
+
 	</div>
 </body>
 </html>
